2a52b41136466563131b2cb3ab5742781d50083b
Andrea <andreabarberio@MBP-di-Andrea.station>
2022-11-22 16:01:50 +0100

    style: add palette options to theme.ts, and contained as default variant for MUIButtons

--- theme.ts
+++ theme.ts
@@ -1,8 +1,27 @@
-import { createTheme } from '@mui/material/styles';
-
+import { grey } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-   
+  palette: {
+    primary: {
+      main: grey[800],
+    },
+    secondary: {
+      main: grey[900],
+      contrastText: "#fff",
+    },
+    grey: {
+      A400: "#bdbdbd",
+    },
+  },
+
+  components: {
+    MuiButton: {
+      defaultProps: {
+        variant: "contained",
+      },
+    },
+  },
 });
 
-export default theme
+export default theme;
